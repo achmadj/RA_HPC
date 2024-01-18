b011f042a8e102ba04771c973a17b07aee227105
achmad <achmadjae.84@gmail.com>
2024-01-18 20:50:55 +0700

    add model and figure

--- 02_deep3D/script/main.py
+++ 02_deep3D/script/main.py
@@ -11,34 +11,42 @@ from torch.utils.data import TensorDataset
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 import datetime
 import time
+from icecream import ic
+
 # get date only
 import sys
 
-def random_split(X, y, train_split, val_split):
+def random_split(X, y, train_split):
   N = X.shape[0]
   idx = np.random.permutation(N)
   train_idx = idx[:int(train_split*N)]
   val_idx = idx[int(train_split*N):]
   return X[train_idx], y[train_idx], X[val_idx], y[val_idx]
 
-def data_preparation(filename, bs=128, N=20000, L=40):
-  with h5py.File(filename, 'r') as hf:
-    feature = hf['feature'][:]
-    target = hf['target'][:]
+def data_preparation(mode, filename, bs=128, N=20000, L=40):
+    with h5py.File(filename, 'r') as hf:
+      feature = hf['feature'][:]
+      target = hf['target'][:]
 
-  X = torch.tensor(feature.reshape(N, 1, L, L, L), dtype=torch.float32)
-  y = torch.tensor(target.reshape(N, 1), dtype=torch.float32)
-  del feature, target
+    X = torch.tensor(feature.reshape(N, 1, L, L, L), dtype=torch.float32)
+    y = torch.tensor(target.reshape(N, 1), dtype=torch.float32)
+    del feature, target
+    
+    if mode == 'train':
+      X_train, y_train, X_val, y_val = random_split(X, y, 0.8)
+      train_set = TensorDataset(X_train, y_train)
+      val_set = TensorDataset(X_val, y_val)
 
-  X_train, y_train, X_val, y_val = random_split(X, y, 0.8, 0.2)
-  train_set = TensorDataset(X_train, y_train)
-  val_set = TensorDataset(X_val, y_val)
+      del X, y
+      train_loader = DataLoader(train_set, batch_size=bs, shuffle=True, num_workers=16)
+      val_loader = DataLoader(val_set, batch_size=bs, shuffle=False, num_workers=16)
 
-  del X, y
-  train_loader = DataLoader(train_set, batch_size=bs, shuffle=True, num_workers=16)
-  val_loader = DataLoader(val_set, batch_size=bs, shuffle=False, num_workers=16)
+      return len(train_set), train_loader, len(val_set), val_loader
 
-  return len(train_set), train_loader, len(val_set), val_loader
+    elif mode == 'test':
+      test_set = TensorDataset(X, y)
+      test_loader = DataLoader(test_set, shuffle=False, num_workers=16)
+      return len(test_set), test_loader
 
 # Create Model
 class AugNet(nn.Module):
@@ -107,17 +115,7 @@ def loop_fn(mode, dataset, dataloader, model, criterion, optimizer, scheduler=No
   scheduler.step(epoch_loss)
   return epoch_loss
 
-if __name__ == '__main__':
-  train_set, train_loader, val_set, val_loader = data_preparation('/clusterfs/students/achmadjae/RA/02_deep3D/data/sho_train_40_15000.h5')
-  NOW = sys.argv[1]
-
-  # MCOC
-  model = AugNet().to('cuda')
-  criterion = nn.L1Loss()
-  optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-  scheduler = ReduceLROnPlateau(
-  optimizer, mode='min', factor=0.1, patience=5, min_lr=1e-6, verbose=True)
-  max_epoch = 300
+def train(model, criterion, optimizer, scheduler, NOW, train_set=None, train_loader=None, val_set=None, val_loader=None, max_epoch=300):
   best_loss = np.inf
   for epoch in range(max_epoch):
     now = time.time()
@@ -127,8 +125,10 @@ if __name__ == '__main__':
     print(f'Epoch: {epoch+1}/{max_epoch}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f} | {(then-now):.3f} s', flush=True)
     if val_loss < best_loss:
       best_loss = val_loss
-      torch.save(model.state_dict(), f'model_{NOW}.pt')
+      torch.save(model.state_dict(), f'/clusterfs/students/achmadjae/RA/02_deep3D/model/model_{NOW}.pt')
 
+def test(model, NOW, val_loader=None):
+  model.load_state_dict(torch.load(f'/clusterfs/students/achmadjae/RA/02_deep3D/model/model_{NOW}.pt'))
   model.eval()
   with torch.no_grad():
     y_true = []
@@ -144,9 +144,33 @@ if __name__ == '__main__':
   y_pred = np.concatenate(y_pred)
 
   c = np.abs(y_pred - y_true)
-  plt.scatter(y_true, y_pred, s=10, c=c, cmap='inferno_r')
+  plt.scatter(y_true, y_pred, s=10, c=c, cmap='viridis_r', rasterized=True)
   plt.colorbar()
   plt.xlabel('True')
   plt.ylabel('Pred')
-  plt.savefig(f'plot_NOW.png')
+  MIN = np.min([y_true, y_pred])
+  MAX = np.max([y_true, y_pred])
+  plt.xlim(MIN, MAX)
+  plt.ylim(MIN, MAX)
+  plt.savefig(f'/clusterfs/students/achmadjae/RA/02_deep3D/fig/plot_{NOW}.svg', bbox_inches='tight')
+  plt.tight_layout()
   plt.show()
+
+if __name__ == '__main__':
+  NOW = sys.argv[1]
+  mode = sys.argv[2]
+  # MCOC
+  model = AugNet().to('cuda')
+  criterion = nn.L1Loss()
+  optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+  scheduler = ReduceLROnPlateau(
+  optimizer, mode='min', factor=0.1, patience=5, min_lr=1e-6, verbose=True)
+
+  if mode == "train":
+    filename = f'/clusterfs/students/achmadjae/RA/02_deep3D/data/{NOW}_train_40_15000.h5'
+    train_set, train_loader, val_set, val_loader = data_preparation(mode, filename)
+    train(model, criterion, optimizer, scheduler, NOW, train_set, train_loader, val_set, val_loader, max_epoch=10)
+  elif mode=="test":
+    filename = f'/clusterfs/students/achmadjae/RA/02_deep3D/data/{NOW}_test_40_5000.h5'
+    test_set, test_loader = data_preparation(mode, filename, N=5000)
+    test(model, NOW, val_loader=test_loader)
