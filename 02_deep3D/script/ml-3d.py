f69ecca1d93bcacf6247cd564efd68ac1a480587
achmad <achmadjae.84@gmail.com>
2024-01-20 07:03:43 +0700

    Add ml-3d.py script for 3D machine learning

---
+++ 02_deep3D/script/ml-3d.py
@@ -0,0 +1,27 @@
+from icecream import ic
+import time
+import numpy as np
+import h5py
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor
+
+filename = '/clusterfs/students/achmadjae/RA/02_deep3D/data/sho_train_40_15000.h5'
+with h5py.File(filename, 'r') as hf:
+    X = hf['feature'][:]
+    y = hf['target'][:]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# flat the X_train and X_test
+X_train = X_train.reshape(X_train.shape[0], -1)
+X_test = X_test.reshape(X_test.shape[0], -1)
+regr = RandomForestRegressor(n_estimators=200, max_depth=80, random_state=42, criterion='absolute_error', n_jobs=-1, max_features=0.6, min_samples_leaf=10)
+now = time.time()
+regr.fit(X_train, y_train)
+then = time.time()
+ic(then-now)
+y_pred = regr.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+ic(mae)
+
